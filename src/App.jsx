8a4acae7bb21d0d718f7e1d3b8af2b8bc0a53c34
Victor Tovar <manuelxdvic@gmail.com>
2023-03-31 22:52:12 -0600

    Add an fuction of js on the code in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ function App() {
 
 
   return (
+    <div>
+
     <div className="App">
-      <h1>Hola mundo</h1>
+      <h1>{'Hola mundo'.toUpperCase()}</h1>
+      <p>Holaa</p>
     </div>
+    </div>
+
   )
 }
 
